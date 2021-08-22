bd10685442513d1c73ea7dfcd9f827bebac874c1
Victor Arreaga <Victor Arreaga>
2021-08-22 17:08:01 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'garden06',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
